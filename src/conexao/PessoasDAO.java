56b451cfacfe221fa6aca713f74a170bcaad9ac1
RBranco1 <raphael.pieroni@aluno.soueinstein.com.br>
2022-05-30 21:36:00 -0300

    DAO, inserir e listar criado

--- src/conexao/PessoasDAO.java
+++ src/conexao/PessoasDAO.java
@@ -10,43 +10,37 @@ import java.util.List;
 
 public class PessoasDAO {
 
-    // Método para retornar lista de carros, como todos os carros
     public List<Pessoa> obterListaDePessoas() throws SQLException {
         try {
-            //Conecta ao banco de dados por meio da classe de conexão
+       
             Conexao con = new Conexao();
             con.getConexao(); //Obtendo a conexão
 
-            // Comando SQL na base = tabela de carros
             String sql = "select * from tb_pessoa;";
 
-            //Executa a query (comando SQL)
-            /*PreparedStatement é usado para criar um objeto que representa a instrução SQL
-            que será executada, sendo que é invocado através do objeto Connection .*/
+           
             PreparedStatement comando = con.getConexao().prepareStatement(sql);
             ResultSet resultado = comando.executeQuery();
 
-            //Prepara a lista de carros para retornar
+           
             List<Pessoa> listaDePessoa;
             listaDePessoa = new ArrayList<>();
 
-            //Para cada item retornado no comando (SQL) faça...
+   
             while (resultado.next()) {
                 Pessoa per = new Pessoa(); //Criando uma instância, novo carro na memória
 
                 per.setId(resultado.getInt("idPessoa")); // Define ID do carro;
                 per.setNome(resultado.getString("nome")); // Define ID do carro;
 
-                // Insere o carro na lista Local
+       
                 listaDePessoa.add(per);
             }
 
-            // Após terminar, fecha a conexão
             resultado.close();
             comando.close();
             con.getConexao().close();
 
-            // Retorna a lista de carros
             return listaDePessoa;
         } catch (SQLException e) { //Caso dê alguma exceção
             System.out.println(e.getMessage());
